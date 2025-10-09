84cd8a1049d9abe3eecc3bd35b00ec2306f996a5
mkenopsia <gxkurro@gmail.com>
2025-10-09 23:27:24 +0700

    initial

---
+++ src/main/java/ru/mkenopsia/kendel/KenDelApplication.java
@@ -0,0 +1,13 @@
+package ru.mkenopsia.kendel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenDelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenDelApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/db_filling.sql
@@ -0,0 +1,20 @@
+insert into shipping_management.t_client (c_fio, c_passport_series_number, c_phone_number, c_status)
+values ('Иванов Василь Павлович',
+        '0420112432',
+        '88005553535',
+        'Клиент');
+insert into shipping_management.t_client (c_fio, c_passport_series_number, c_phone_number, c_status)
+values ('Стариков Макс Алексеевич',
+        '0419442435',
+        '89509494356',
+        'Клиент');
+insert into shipping_management.t_client (c_fio, c_passport_series_number, c_phone_number, c_status)
+values ('Гранин Егор Вадимович',
+        '0439442435',
+        '89504324540',
+        'Клиент');
+insert into shipping_management.t_client (c_fio, c_passport_series_number, c_phone_number, c_status)
+values ('Константинов Владимир Сергеевич',
+        '0309314295',
+        '895048769013',
+        'Партнёр');
